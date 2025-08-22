d7a5b602276548c69e6d29d624262c7ca4ae03c9
Arun Bonal <arunbonal516@gmail.com>
2025-08-22 19:55:53 +0530

    changed cvServer logic

--- cvServer_og_backup.py
+++ cvServer_og_backup.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 import cv2
 import easyocr
 import numpy as np
-import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -25,6 +24,5 @@ def upload():
     return jsonify({"text": texts})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
-    print(f"Server is running on port {port}")
-    app.run(debug=True, host='0.0.0.0', port=port)
+    print("Server is running on port 3000")
+    app.run(debug=True, host='0.0.0.0', port=3000)  # Running on port 3000
--- pyServer/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Install Python dependencies
-pip install -r requirements.txt 
